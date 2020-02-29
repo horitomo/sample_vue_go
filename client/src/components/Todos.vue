9cc433abe7eec6dfbc7074a3000b8669bdc0011b
堀田知生 <tomoki@cocacola.local>
2020-02-29 19:43:03 +0900

    todoの内容を表記する

---
+++ client/src/components/Todos.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        {{ this.item.id }}
+        {{ this.item.Text }}
+        {{ this.item.status }}
+    </div>
+</template>
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
