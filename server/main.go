6cc61cbe275e222c4f1b6f7012dcf81cce5a2e7a
堀田知生 <tomoki@cocacola.local>
2020-03-02 15:24:00 +0900

    /loginを作成

--- server/main.go
+++ server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,18 +10,34 @@ type clientValue struct {
 	Msg string `json:"msg"`
 }
 
+type clientLoginValue struct {
+	ID       string `json:"id"`
+	Password string `json:"password"`
+}
+
 func main() {
-    r := gin.Default()
-    r.POST("/api", func(c *gin.Context) {
+	r := gin.Default()
+	r.POST("/api", func(c *gin.Context) {
 		var client clientValue
 		c.BindJSON(&client)
 		log.Println(client.Msg)
 		log.Println("hogehoge")
 		c.Header("Access-Control-Allow-Origin", "*")
-        c.JSON(200, gin.H{
+		c.JSON(200, gin.H{
 			"message": "ping",
 			"request": client.Msg,
 		})
-    })
-    r.Run(":8888")
+	})
+
+	r.POST("/login", func(c *gin.Context) {
+		var loginForm clientLoginValue
+		c.BindJSON(&loginForm)
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.JSON(200, gin.H{
+			"message": "ping",
+			"request": loginForm,
+		})
+	})
+
+	r.Run(":8888")
 }
