bb82967f74f21501704fa62af8aee512413b7cf7
堀田知生 <tomoki@cocacola.local>
2020-03-02 15:22:49 +0900

    loginのルート作成

--- client/src/router.js
+++ client/src/router.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 // ページコンポーネントをインポートする
+import Index from './pages/Index.vue'
+
 import Login from './pages/Login.vue'
 
 // VueRouterプラグインを使用する
@@ -12,12 +14,20 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    component: Login
+    component: Index
+  },
+  {
+    path : "/login",
+    component : Login
   }
 ]
 
 // VueRouterインスタンスを作成する
 const router = new VueRouter({
+  mode: 'history',
+  scrollBehavior () {
+    return { x: 0, y: 0 }
+  },
   routes
 })
 
