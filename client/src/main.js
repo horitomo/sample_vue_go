8886a1a679e5f1f4a4f21ef377e71bf2d316a5a2
堀田知生 <tomoki@cocacola.local>
2020-03-05 21:26:17 +0900

    cssとvuex追加

--- client/src/main.js
+++ client/src/main.js
@@ -2,11 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import router from './router'
+import Vuex from 'vuex'
+
+require('@/assets/sass/main.scss')
 
 import axios from 'axios' //追記
 import VueAxios from 'vue-axios' //追記
 
-Vue.use(VueAxios, axios) //追記
+Vue.use(VueAxios, axios, Vuex) //追記
 
 Vue.config.productionTip = false
 
