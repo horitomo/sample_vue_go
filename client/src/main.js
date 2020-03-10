61a14885cbd48a5cbcc49d5fdf82c8873279452a
堀田知生 <tomoki@cocacola.local>
2020-03-10 13:33:44 +0900

    vuex,storeの使用を追記

--- client/src/main.js
+++ client/src/main.js
@@ -2,18 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import router from './router'
-import Vuex from 'vuex'
+import store from './store'
 
 require('@/assets/sass/main.scss')
 
 import axios from 'axios' //追記
 import VueAxios from 'vue-axios' //追記
 
-Vue.use(VueAxios, axios, Vuex) //追記
+Vue.use(VueAxios, axios) //追記
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
